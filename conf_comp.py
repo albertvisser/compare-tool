474080d481176013cd3a698ec2cd52c80f2be11b
Albert Visser <albert@lemoncurry>
2016-08-27 14:04:41 +0200

    new version: Python3/PyQt support, introducing xml comparison

--- conf_comp.py
+++ conf_comp.py
@@ -1,308 +1,121 @@
 import sys
 import os
+import collections
+import pprint
 # vergelijk entries in config file
 
-# 1. toon schermpje waarin je de twee files kunt kiezen
-# lees alle entries onder alle headers
-# per header: sorteer de entries en zet ze daarna naast elkaar
+from configparser import ConfigParser, MissingSectionHeaderError
 
-# subroutine: lees ini file
-from ConfigParser import SafeConfigParser, MissingSectionHeaderError
+def sort_inifile(fn):
+    """define a generator that yields the sorted options
+    """
+    test = ConfigParser(allow_no_value=True, interpolation=None)
+    test.read(fn)
+    for sect in sorted(test.sections()):
+    ## for sect in test.sections():
+        for opt in sorted(test.options(sect)):
+        ## for opt in test.options(sect):
+            ## print(sect, opt, test[sect][opt])
+            yield sect, opt, test[sect][opt]
 
-class MyNewConfigParser:
-    def __init__(self):
-        self.sections = {}
-        self.garbage = []
-        self.inSection = False
-        self.inOption = False
+def check_inifile(fn):
+    """copy to a temporary file, making sure first line contains a section header
 
-    def read(self,fn):
-        section = {}
-        garb = []
-        for x in file(fn,"r"):
-            h = x[:-1]
-            if h == "":
-                continue
-            if h.startswith("[") and h.endswith("]"):
-                if len(section) > 0:
-                    self.sections[secname] = section
-                    section = {}
-                if len(garb) > 0:
-                    self.garbage.append(garb)
-                    garb = []
-                secname = h[1:-1]
-                self.inSection = True
-                continue
-            elif h.startswith("#") or h.startswith(";"):
-                garb.append(h)
-            elif not self.inSection:
-                garb.append(h)
-            else:
-                s = h.split("=",1)
-                if len(s) == 1:
-                    s.append("")
-                section[s[0]] = s[1]
-        if len(section) > 0:
-            self.sections[secname] = section
-        if len(garb) > 0:
-            self.garbage.append(garb)
+    returns the name of the temporary file
+    """
+    hlpfn = os.path.join('/tmp', os.path.basename(fn))
+    with open(fn,encoding='latin-1') as f_in, open(hlpfn, 'w') as f_out:
+        first = True
+        for line in f_in:
+            if first:
+                if not line.startswith('['):
+                    f_out.write('[  --- generated first header ---  ]\n')
+                first = False
+            f_out.write(line)
+    return hlpfn
 
-
-class MyConfigParser:
-    def __init__(self):
-        self.volgorde = [] # sections en begin- en eindflauwekul
-        self.options = {}
-        self.thisSection = None
-
-    def read(self,fn):
-        f = file(fn,"r")
-        self.inSection = False
-        self.inOption = False
-        self.endOption = False
-        self.thisOption = []
-        for x in f.readlines():
-            if x[0] == "#" or x[0] == ";":
-                if self.inOption:
-                    self.thisOption.append(x[:-1])
-                elif self.inSection:
-                    self.thisSection.append(x[:-1])
-            elif not self.inSection:
-                self.volgorde.append(x[:-1])
-            elif x[0] == "[":
-                self.newSection(x)
-            else:
-                h = x[:-1].split("=")
-                if len(h) == 1:
-                    h = x[:-1].split(": ")
-                if len(h) == 1:
-                    self.thisOption.append(x)
-                else:
-                    naam = h[0]
-                    j = h[1].split("#")
-                    if len(j) == 1:
-                        j = h[1].split(";")
-                    if len(j) == 1:
-                        waarde = j
-                        cmt = ''
-                    else:
-                        waarde = j[0]
-                        cmt = j[0]
-                    self.thisSection["volgorde"].append(naam)
-                    self.thisSection[naam] = (self.thisOption,waarde,cmt)
-                    self.inOption = False
-            if self.thisSection != None:
-                self.addSection()
-
-    def newSection(self,x):
-        if self.thisSection != None:
-            self.addSection()
-        self.inSection = True
-        self.inOption = True
-        i = x.find("]")
-        h = x[1:i]
-        self.thisSection = {"naam": h,"volgorde": []}
-        h = x[i+1:]
-        self.thisSection["regelrest"] = h
-
-    def addSection(self):
-        d = {}
-        for x in self.thisSection["volgorde"]:
-            d[x] = self.thisSection[x]
-        self.options[self.thisSection["naam"]]= (self.thisSection["volgorde"],d)
-        self.volgorde.append(self.thisSection["naam"])
-
-
-
-class iniobj(SafeConfigParser):
-    def __init__(self,fn):
-        self.dlm = "#$#"
-        self.fn = fn
-        SafeConfigParser.init()
-
-    def read():
-        SafeConfigParser.read(self.fn)
-
-    def list(self):
-        print self.fnaam
-
-    def write(self):
-        f = file(self.fn,"W")
-        SafeConfigParser.write(f)
-
-class readini:
-    def __init__(self,fn):
-        self.dlm = "#$#"
-        self.mld = None
-        self.sections = {}
-        # inlezen mru-gegevens
-        s = MyNewConfigParser() # was SafeConfigParser()
+def build_options_list(fn):
+    """return a sorted list of options
+    """
+    result = []
+    gen = sort_inifile(fn)
+    while True:
         try:
-            s.read(fn)
-        except MissingSectionHeaderError, mld:
-            self.mld = mld
-            return
+            result.append(next(gen))
+        except StopIteration:
+            break
+    return result
+
+def compare_configs(fn1, fn2):
+    """compare two ini files
+    """
+    result = []
+    fn1 = check_inifile(fn1)
+    fn2 = check_inifile(fn2)
+    gen1 = sort_inifile(fn1)
+    gen2 = sort_inifile(fn2)
+    def gen_next(gen):
+        eof = False
+        try:
+            sect, opt, val = next(gen)
+        except StopIteration:
+            eof = True
+            sect = opt = val = ''
+        return eof, sect, opt, val
+    current_sect = current_opt = ''
+    eof_gen1, sect1, opt1, val1 = gen_next(gen1)
+    eof_gen2, sect2, opt2, val2 = gen_next(gen2)
+    # nog leuke trucjes met itertools mogelijk?
+    while True:
+        get_from_1 = get_from_2 = False
+        ## print((sect1, opt1), (sect2, opt2))
+        if (eof_gen1, sect1) < (eof_gen2, sect2):
+            result.append(((sect1, opt1), val1, ''))
+            if not eof_gen1:
+                get_from_1 = True
+        elif (eof_gen1, sect1) > (eof_gen2, sect2):
+            result.append(((sect2, opt2), '', val2))
+            if not eof_gen2:
+                get_from_2 = True
         else:
-            pass
-        sc = s.sections.keys() # was s.sections()
-        sc.sort()
-        for x in sc:
-            op = s.sections[x].keys() # was s.options(x)
-            op.sort()
-            opval = {}
-            for y in op:
-                v = s.sections[x][y] # was s.get(x,y)
-                opval[y] = v
-            self.sections[x] = opval
+            if opt1 < opt2:
+                result.append(((sect1, opt1), val1, ''))
+                get_from_1 = True
+            elif opt1 > opt2:
+                result.append(((sect2, opt2), '', val2))
+                get_from_2 = True
+            else:
+                result.append(((sect1, opt1), val1, val2))
+                get_from_1 = True
+                get_from_2 = True
+        if get_from_1:
+            eof_gen1, sect1, opt1, val1 = gen_next(gen1)
+        if get_from_2:
+            eof_gen2, sect2, opt2, val2 = gen_next(gen2)
+        if eof_gen1 and eof_gen2:
+            break
+    return result
 
-    def listkeys(self):
-        self.options = []
-        h = self.sections.keys()
-        h.sort()
-        for x in h:
-            self.options.append("%ssection%s%s" % (self.dlm,self.dlm,x))
-            hh = self.sections[x].keys()
-            hh.sort()
-            for y in hh:
-                self.options.append("%soption%s%s%s%s" % (self.dlm,self.dlm,y,self.dlm,self.sections[x][y]))
-
-class inicomp:
-    def __init__(self,f1,f2):
-        self.ft = ''
-        self.olist = []
-        self.rlist = []
-        self.llist = []
-        h1 = readini(f1)
-        if h1.mld is not None:
-            self.ft = ('bestand "%s" is geen leesbaar ini file voor deze procedure' % f1)
-            return
-        h1.listkeys()
-        #~ print h1.dlm,f1,h1.dlm
-        #~ for x in h1.options:
-            #~ print x
-        h2 = readini(f2)
-        if h2.mld is not None:
-            self.ft =  ('bestand "%s" is geen leesbaar ini file voor deze procedure' %  f2)
-            return
-        h2.listkeys()
-        #~ print h2.dlm,f2,h2.dlm
-        #~ for x in h2.options:
-            #~ print x
-        i1 = 0
-        i2 = 0
-        while i1 < len(h1.options) and i2 < len(h2.options):
-            s1 = h1.options[i1].split(h1.dlm)
-            x1 = ":".join(s1[1:3])
-            s2 = h2.options[i2].split(h2.dlm)
-            x2 = ":".join(s2[1:3])
-            if x1 < x2:
-                if len(s1) < 4:
-                    self.olist.append('[%s]' % s1[2])
-                    self.llist.append('')
-                    self.rlist.append('')
-                    #~ r = ("%s: %s" % (s1[1],s1[2]))
-                else:
-                    self.olist.append(s1[2])
-                    self.llist.append(s1[3])
-                    self.rlist.append('')
-                    #~ r = ("  %s %s: %s" % (s1[1],s1[2],s1[3]))
-                i1 += 1
-            elif x1 > x2:
-                if len(s2) < 4:
-                    self.olist.append('[%s]' % s2[2])
-                    self.llist.append('')
-                    self.rlist.append('')
-                    #~ r = ("%s:                                               %s" % (s2[1],s2[2]))
-                else:
-                    self.olist.append(s2[2])
-                    self.llist.append('')
-                    self.rlist.append(s2[3])
-                    r = ("  %s                                              %s: %s" % (s2[1],s2[2],s2[3]))
-                i2 += 1
-            if x1 == x2:
-                if len(s1) < 4:
-                    self.olist.append('[%s]' % s1[2])
-                    self.llist.append('')
-                    self.rlist.append('')
-                    #~ r = ("%s: %s                  %s" % (s1[1],s1[2],s2[2]))
-                else:
-                    self.olist.append(s1[2])
-                    self.llist.append(s1[3])
-                    self.rlist.append(s2[3])
-                    #~ r = ("  %s %s: %s             %s: %s" % (s1[1],s1[2],s1[3],s2[2],s2[3]))
-                i1 += 1
-                i2 += 1
-
-def test_afrift():
-    f1 = "afrift.ini"
-    f2 = "afriftftc.ini"
-    c = inicomp(f1,f2)
-    for x in range(len(c.olist)):
-        i = x # - 1
-        print ('%20s %40s %40s' % (c.olist[i],c.llist[i],c.rlist[i]))
-
-def test_myCP(fn):
-    my = MyConfigParser()
-    my.read(fn)
-##    print "volgorde:"
-##    for x in my.volgorde:
-##        print "\t",x
-##    print "options: "
-##    for x in my.options.keys():
-##        print "\t",x,my.options[x]
-    return my
-
-def test_myNCP(fn):
-    my = MyNewConfigParser()
-    my.read(fn)
-    return my
-
-def test_readini(fn):
-    my = readini(fn)
-    ## if my.mld is not None:
-        ## print my.mld
-##        self.ft = ('bestand "%s" is geen leesbaar ini file voor deze procedure' % f1)
-        ## return
-##    my.listkeys()
-    return my
-
-def schrijf_test(my,out):
-    o = open(out,"w")
-    for x in my.__dict__.keys():
-        o.write(x + "\n")
-        h = my.__dict__[x]
-        if type(h) is list:
-            for y in h:
-                o.write(str(y).join(("\t","\n")))
-        elif type(h) is dict:
-            for y in h.keys():
-                o.write(str(y).join(('\t',"\n")))
-                if type(h[y]) is dict:
-                    for z in h[y].keys():
-                        o.write(str(z).join(('\t\t',"\n")))
-                        o.write(str(h[y][z]).join(("\t\t\t","\n")))
-                else:
-                    o.write(str(h[y]).join(("\t\t","\n")))
-        else:
-            o.write(str(my.__dict__[x]).join(("\t","\n")))
-    o.close()
 
 if __name__ == "__main__":
-    fn = "wincmd.ini"
-    out = os.path.split(fn)[1] + "_myNCP.txt"
-    my = test_myNCP(fn)
-    schrijf_test(my,out)
-    out = os.path.split(fn)[1]  + "_myCP.txt"
-    my = test_myCP(fn)
-    schrijf_test(my,out)
-    sys.exit()
-    for fn in ("opera6_V76.ini","testing.ini","afrift.ini"):
-        ## out = fn + "_readini.txt"
-        ## my = test_readini(fn)
-        ## schrijf_test(my,out)
-        out = fn + "_myNCP.txt"
-        my = test_myNCP(fn)
-        schrijf_test(my,out)
-        out = fn + "_myCP.txt"
-        my = test_myCP(fn)
-        schrijf_test(my,out)
-##    test_afrift()
+    f1 = "actif.ini"
+    ## f1 = check_inifile(f1)
+    ## pprint.pprint(build_options_list(f1))
+    ## print()
+    f2 = "testing.ini"
+    ## f2 = check_inifile(f2)
+    ## pprint.pprint(build_options_list(f2))
+    ## print()
+    f2 = "jvscompare.ini"
+    pprint.pprint(compare_configs(f1, f2))
+    ## compare_configs(f1, f2)
+    ## fn = "wincmd.ini"
+    ## pprint.pprint(build_options_list(fn))
+
+    ## test_afrift(f1, f2)
+    ## test_readini(f1)
+    ## test_readini(f2)
+    ## fn = "wincmd.ini"
+    ## test_myNCP(fn)
+    ## test_myCP(fn)
+    ## test_readini(fn)
---
+++ xml_comp.py
@@ -0,0 +1,145 @@
+import xml.etree.ElementTree as et
+import collections
+import pprint
+
+"""
+
+1e kolom: tree position
+2e kolom: value left
+3e kolom: value right
+
+xml: element tree - uitbreiden met attribuut naam bv
+bij <x><y1 a1='1' a2='2'></y1><y2><z1>gragl</z1></y2></x>
+(((x, y1), ),'', '')
+(((x, y1), a1),'1', '')
+(((x, y1), a2),'2', '')
+(((x, y2, z1), ),'gargl', '')
+"""
+
+def sort_xmldata(fn):
+    """sorteert op elementnaam en daarbinnen op waarde van het eerste element
+    (instelbaar maken zodat je kunt kiezen op welk attribuut geordend wordt)
+    """
+    def getattrval(element):
+        default = retval = ''
+        for attr, val in element.items():
+            if not default:
+                default = val
+            for name in ('id', 'name'):
+                if attr == name:
+                    retval = val
+                    break
+        if not retval:
+            retval = default
+        return retval
+    def process_subelements():
+        nonlocal result, current_element_list
+        current_element = current_element_list[-1]
+        subelements = sorted(set(x.tag for x in list(current_element)))
+        for name in subelements:
+            ## print(name)
+            nodevalue = [x.tag for x in current_element_list] + [name]
+            element_list = current_element.findall(name)
+            if len(element_list) > 1:
+                ## for element in element_list:
+                    ## print(element.text, [x for x in element.items()])
+                ## print('---')
+                # nu eerst subelementen in volgorde leggen maw element_list herordenen
+                ## element_list = sorted(element_list, key=lambda x: x.items()[0][1])
+                element_list = sorted(element_list, key=getattrval)
+            for element in element_list:
+                ## print(element.text, [x for x in element.items()])
+                attr_name = ''
+                datavalue = element.text
+                if datavalue is not None:
+                    datavalue = datavalue.strip()
+                result.append((nodevalue, attr_name, datavalue))
+                for attr in sorted(element.items()):
+                    attr_name, datavalue = attr
+                    result.append((nodevalue, attr_name, datavalue))
+                current_element_list.append(element)
+                retlist = process_subelements()
+                current_element_list.pop()
+    result = []
+    tree = et.parse(fn)
+    root = tree.getroot()
+    # build the root element for the comparison list
+    nodevalue = [root.tag,]         # elements
+    if root.items():
+        attr_name, datavalue = root.items()[0]  # name/value of (in this case first) attribute
+    else:
+        attr_name = datavalue = ''
+    result.append((nodevalue, attr_name, datavalue))
+    # get a sorted list of the subelement names
+    current_element_list = [root]
+    process_subelements()
+
+
+    return result
+
+def compare_xmldata(fn1, fn2):
+    """compare two similar xml documents
+    """
+    result = []
+    gen1 = (x for x in sort_xmldata(fn1))
+    gen2 = (x for x in sort_xmldata(fn2))
+    def gen_next(gen):
+        eof = False
+        try:
+            elem, attr, val = next(gen)
+        except StopIteration:
+            eof = True
+            elem = attr = val = ''
+        return eof, elem, attr, val
+    current_elem = current_attr = ''
+    eof_gen1, elem1, attr1, val1 = gen_next(gen1)
+    eof_gen2, elem2, attr2, val2 = gen_next(gen2)
+    while True:
+        get_from_1 = get_from_2 = False
+        ## print((sect1, opt1), (sect2, opt2))
+        # probleem:dit werkt niet als de elem lijsten ongelijk van lengte zijn want dan
+        # is de langste eigenlijk de "kleinste"
+        if (eof_gen1, elem1) < (eof_gen2, elem2):
+            result.append(((elem1, attr1), val1, ''))
+            if not eof_gen1:
+                get_from_1 = True
+        elif (eof_gen1, elem1) > (eof_gen2, elem2):
+            result.append(((elem2, attr2), '', val2))
+            if not eof_gen2:
+                get_from_2 = True
+        else:
+            if attr1 < attr2:
+                result.append(((elem1, attr1), val1, ''))
+                get_from_1 = True
+            elif attr1 > attr2:
+                result.append(((elem2, attr2), '', val2))
+                get_from_2 = True
+            else:
+                result.append(((elem1, attr1), val1, val2))
+                get_from_1 = True
+                get_from_2 = True
+        if get_from_1:
+            eof_gen1, elem1, attr1, val1 = gen_next(gen1)
+        if get_from_2:
+            eof_gen2, elem2, attr2, val2 = gen_next(gen2)
+        if eof_gen1 and eof_gen2:
+            break
+    return result
+
+
+def main():
+    ## pprint.pprint(compare_xmldata('.lmmsrc.xml', '.lmmsrc_o.xml'))
+    ## pprint.pprint(sort_xmldata('.lmmsrc.xml'))
+    with open('bezettingen_current', 'w') as _o:
+        pprint.pprint(sort_xmldata('/home/albert/magiokis/data/songs/'
+            'magiokis_songs_xmldata/Bezettingen.xml'), stream=_o)
+    with open('bezettingen_backup', 'w') as _o:
+        pprint.pprint(sort_xmldata('/home/albert/magiokis/data/songs/'
+            'magiokis_songs_xmldata/Bezettingen.xml.bak'), stream=_o)
+    with open('bezettingen_compare', 'w') as _o:
+        pprint.pprint(compare_xmldata('/home/albert/magiokis/data/songs/'
+            'magiokis_songs_xmldata/Bezettingen.xml', '/home/albert/magiokis/'
+            'data/songs/magiokis_songs_xmldata/Bezettingen.xml.bak'), stream=_o)
+
+if __name__ == '__main__':
+    main()
