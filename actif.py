13d26f3dca84e13695ecd15d7800528ce358d338
Albert Visser <albert.visser@gmail.com>
2019-11-08 20:02:00 +0100

    added import for wx version (commented)

--- actif.py
+++ actif.py
@@ -2,9 +2,9 @@
 """starter for Compare Tool
 """
 import argparse
-## from actif_qt4 import main
 from actif_shared import comparetypes
 from actif_qt import main
+# from actif_wx import main
 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('-m', '--method', choices=comparetypes, help='comparison method')
